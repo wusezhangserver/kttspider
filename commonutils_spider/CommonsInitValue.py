d14e1408fcc712a64e726d7d716e99372c7cd1b1
wusezhangserver <393124085@qq.com>
2015-04-10 17:52:55 +0800

    资源变更整理

--- commonutils_spider/CommonsInitValue.py
+++ commonutils_spider/CommonsInitValue.py
@@ -1,7 +1,14 @@
 import time
+import datetime
 
 def initTempImage():
     return 'http://216.189.56.159/imagelib/iconresource/editor/2x_web/ic_insert_invitation_grey600_36dp.png'
 
 def initNowTime():
     return  time.strftime("%Y-%m-%d %X",time.localtime())
+
+def initBeforeDayTime():
+    now_time = datetime.datetime.now()
+    befor_time = now_time + datetime.timedelta(days=-1)
+    yes_time_format =  befor_time.strftime('%Y-%m-%d')
+    return yes_time_format
