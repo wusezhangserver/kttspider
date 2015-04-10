8f55395a423bc6ce9c5f834f24e12e561df36b3b
wusezhangserver <393124085@qq.com>
2015-04-10 17:33:44 +0800

    资源变更整理

---
+++ commonutils_spider/CommonsInitValue.py
@@ -0,0 +1,2 @@
+def initTempImage():
+    return 'http://216.189.56.159/imagelib/iconresource/editor/2x_web/ic_insert_invitation_grey600_36dp.png'
--- mornnews_spider/YiCaiStockNetSpider.py
+++ mornnews_spider/YiCaiStockNetSpider.py
@@ -1,6 +1,9 @@
 from commonutils_spider import  CommonsMysqlUtils
+from commonutils_spider import  CommonsInitValue
 from  selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+import time
+import uuid
 
 def crawYiCaiStockDailyNews(link):
     currentArray = []
@@ -8,25 +11,32 @@ def crawYiCaiStockDailyNews(link):
     browsor.get(link)
     contextArray = browsor.find_elements_by_tag_name('dl')
     for context in contextArray:
-        title = context.find_element_by_tag_name('h1').text
-        print title
+        try:
+          titleValue = context.find_element_by_tag_name('h1')
+          descriptContext = context.find_element_by_tag_name('p').text
+          pubDate = time.strftime("%Y-%m-%d %X",time.localtime())
+          linkUrl = context.find_element_by_tag_name('a').get_attribute('href')
+          try:
+              imageObj = context.find_element_by_tag_name('img')
+              imageUrl = imageObj.get_attribute('src')
+          except NoSuchElementException,e:
+              imageUrl = CommonsInitValue.initTempImage()
 
-
-
-
-
-    #currentList.append([str(uuid.uuid1()),linkUrl,topImageUrl,title,pubDate,descriptContext,'STOCK','YICAINET'])
+        except NoSuchElementException,e:
+              continue
+        title = titleValue.text
+        currentArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'STOCK','YICAINET'])
     return currentArray
     
 def writeYiCaiStockDataSource():
-    link = 'http://www.yicai.com/stock/'
+    link = 'http://www.yicai.com/markets/'
     currentArray = crawYiCaiStockDailyNews(link)
+    dbManager = CommonsMysqlUtils._dbManager
     SQL = " DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE" \
            "  WHERE  SOURCEFLAG = 'YICAINET' AND  NEWSFLAG='STOCK'"
+    dbManager.executeUpdateOrDelete(SQL)
 
     formatSQL = 'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE ' \
                 '(KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG) ' \
                 ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
-
-if __name__ == '__main__':
-    writeYiCaiStockDataSource()
+    dbManager.executeManyInsert(formatSQL,currentArray)
