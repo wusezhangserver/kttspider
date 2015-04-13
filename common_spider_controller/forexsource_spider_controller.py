9ec24e60b1fe3a20e84c63d6498153d80599876b
wusezhangserver <393124085@qq.com>
2015-04-13 12:16:17 +0800

    资源变更整理

--- common_spider_controller/forexsource_spider_controller.py
+++ common_spider_controller/forexsource_spider_controller.py
@@ -1,9 +1,6 @@
-import  sys
-sys.path.append("../cnforex_spider/")
-import  CnforexSpider
-import  SwissquoteSpider
 from distutils import log
-
+from forexpic_spider import  CnforexSpider
+from forexpic_spider import SwissquoteSpider
 def  updateDailyForexPic():
     log.info('The system crawling the resource of forex picture ')
     print '----START CRAW THE FOREX PICTURE----'
