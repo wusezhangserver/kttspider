1c2cf4d5962d423435a4c7fbb4d8d6e315689391
wusezhangserver <393124085@qq.com>
2015-04-14 16:03:51 +0800

    资源变更整理

--- common_spider_controller/forexpic_spider_controller.py
+++ common_spider_controller/forexpic_spider_controller.py
@@ -1,14 +1,12 @@
 from distutils import log
 from forexpic_spider import  CnForexImageSpider
-from forexpic_spider import SwissquoteSpider
+
 def  updateDailyForexPic():
+
     log.info('The system crawling the resource of forex picture ')
     print '----START CRAW THE FOREX PICTURE----'
     CnForexImageSpider.findForexImage();
     
     print '----START CRAW THE DAILY NEWS OF FOREX ----'
     log.info('The system crawling the resource of daily news of  forex ')
-    SwissquoteSpider.writeSwissquoteTodayNews();
-    
-if __name__=="__main__":
-    updateDailyForexPic()
+    #SwissquoteSpider.writeSwissquoteTodayNews();
--- mainController.py
+++ mainController.py
@@ -2,13 +2,16 @@ from common_spider_controller import morningnews_spider_controller
 from common_spider_controller import headline_spider_controller
 from common_spider_controller import dailyblog_spider_controller
 from common_spider_controller import dailytheme_spider_controller
+from common_spider_controller import forexpic_spider_controller
 
 if __name__ == '__main__':
    print '********************************start spider  morningnews********************'
    morningnews_spider_controller.crawDailyNews()
    print '********************************start spider headlingnews********************'
    headline_spider_controller.crawDataCenter()
-   print '********************************start spider dailyblog********************'
+   print '********************************start spider dailyblog************************'
    dailyblog_spider_controller.updateBlogData()
-   print '********************************start spider dailytheme********************'
+   print '********************************start spider dailytheme***********************'
    dailytheme_spider_controller.crawThemeNews()
+   print '********************************start spider forexpic************************'
+   forexpic_spider_controller.updateDailyForexPic()
