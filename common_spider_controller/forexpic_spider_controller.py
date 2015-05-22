f5096a804decbaef68b83bcf7b804c0bf3bef944
wusezhangserver <393124085@qq.com>
2015-05-22 10:01:55 +0800

    资源变更整理

--- common_spider_controller/forexpic_spider_controller.py
+++ common_spider_controller/forexpic_spider_controller.py
@@ -1,16 +1,27 @@
 from distutils import log
 from forexpic_spider import CnForexImageSpider
 from forexpic_spider import HeXunForexImageSpider
+from commonutils_spider import CommonsRecodeErrorUtils
+from commonutils_spider import CommonsInitValue
+import uuid
 
 def updateDailyForexPic():
+
+    currentList = []
+    currentTime = CommonsInitValue.initNowTime()
 
     log.info('The system crawling the resource of forex picture ')
     print '----START CRAW THE FOREX PICTURE----'
     CnForexImageSpider.writeForexImages()
 
     print '----START CRAW THE XEHUN PICTURE----'
-    HeXunForexImageSpider.writeHeXunForexImage()
+    try:
+        HeXunForexImageSpider.writeHeXunForexImage()
+    except Exception,e:
+        currentList.append([currentTime,str(uuid.uuid1()),'HeXunForexImageSpider.writeHeXunForexImage',e])
 
     print '----START CRAW THE DAILY NEWS OF FOREX ----'
     log.info('The system crawling the resource of daily news of  forex ')
     #SwissquoteSpider.writeSwissquoteTodayNews();
+
+    CommonsRecodeErrorUtils.commonRedcodeError(currentList)
