5a64ee41314857d6f295cee10a6397dcfa6da6d6
wusezhangserver <393124085@qq.com>
2015-06-02 18:11:05 +0800

    资源变更整理

--- comment_spider/AdsNetSpider.py
+++ comment_spider/AdsNetSpider.py
@@ -1,40 +1,35 @@
-import AdsNetSpiderUtils
-import time
+from commonutils_spider import CommonsMysqlUtils
+from commonutils_spider import CommonsInitValue
+from selenium import webdriver
 import uuid
 
 def crawDailyComments(link):
-    startContext = AdsNetSpiderUtils.returnStartContext(link, '<div class="media-list">')
-    len = AdsNetSpiderUtils.findAllTarget(startContext,'<div class="node-title">')
+
     currentList = []
-    for i  in range(len):
-        targetContext = AdsNetSpiderUtils.divisionTarget(startContext,'<div class="node-title">','</article>')
-        startContext = targetContext['nextContext']
-        currentcontext =  targetContext['targetContext']
-        title = AdsNetSpiderUtils.filterContextByTarget(currentcontext,'<h2>','</h2>')
-        descriptContext = AdsNetSpiderUtils.filterContextByTarget(currentcontext,'<div class="node-content">','</article>')
-        descriptContext = AdsNetSpiderUtils.removeSpecialCharacter(descriptContext)
-        pubDate = time.strftime("%Y-%m-%d %X",time.localtime())
-        currentList.append([str(uuid.uuid1()),'',title,pubDate,descriptContext,'FOREX','ADSNET'])
+    browsor = webdriver.PhantomJS()
+    browsor.get(link)
+    contextList = browsor.find_elements_by_class_name('news-item')
+    for mainContext in contextList:
+        pubDate = CommonsInitValue.initNowTime()
+        title = mainContext.find_element_by_tag_name('a').text
+        linkUrl = mainContext.find_element_by_tag_name('a').get_attribute('href')
+        descriptContext = mainContext.find_element_by_class_name('desc').text
+        currentList.append([str(uuid.uuid1()),linkUrl,title,pubDate,descriptContext,'FOREX','ADSNET'])
     return currentList
 
 
 def writeDailyComments():
-    link = 'http://www.ads-securities.com/zhs/market-research/market-news'
-    currentList = crawDailyComments(link);
-    conn = AdsNetSpiderUtils.getMySQLConn()
-    cursor = conn.cursor()
-    try:
-        cursor.execute("DELETE  FROM  COMMENTS_NEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'ADSNET'")
-        conn.commit()
-    except conn.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        conn.rollback()
-    formatSQL = 'INSERT COMMENTS_NEWS_RESOURCE_TABLE (KEYID,LINKURL,TITLE,PUBDATE,DESCRIPTCONTEXT,COMMENTFLAG,SOURCEFLAG) VALUES (%s,%s,%s,%s,%s,%s,%s)'
-    try:
-        cursor.executemany(formatSQL,currentList)
-        conn.commit()
-    except conn.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        conn.rollback()
-    cursor.close()
-    conn.close()
+    link = 'http://www.ads-securities.com/zhs/market-research'
+    currentArray = crawDailyComments(link)
+
+    dbManager = CommonsMysqlUtils._dbManager
+    SQL = "DELETE  FROM  COMMENTS_NEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'ADSNET' AND COMMENTFLAG='FOREX' "
+    dbManager.executeUpdateOrDelete(SQL)
+
+    formatSQL = ' INSERT COMMENTS_NEWS_RESOURCE_TABLE ' \
+                ' (KEYID,LINKURL,TITLE,PUBDATE,DESCRIPTCONTEXT,COMMENTFLAG,SOURCEFLAG)' \
+                ' VALUES (%s,%s,%s,%s,%s,%s,%s)'
+    dbManager.executeManyInsert(formatSQL,currentArray)
+
+if __name__ == '__main__':
+    writeDailyComments()
