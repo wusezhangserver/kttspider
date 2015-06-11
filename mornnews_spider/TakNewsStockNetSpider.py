aaeec05167c9ad4ae338a5e9f35f12d10b099ce7
wusezhangserver <393124085@qq.com>
2015-06-11 13:43:09 +0800

    资源变更整理

--- mornnews_spider/TakNewsStockNetSpider.py
+++ mornnews_spider/TakNewsStockNetSpider.py
@@ -8,19 +8,25 @@ def crawFinanceHLDataSource(link):
     browsor = webdriver.PhantomJS()
     browsor.get(link)
     mainlist = browsor.find_element_by_class_name('news_list').find_elements_by_class_name('list')
-
-
-    #currentList.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'STOCK','TAKCHINA'])
+    for context in mainlist:
+        imageUrl = context.find_element_by_tag_name('img').get_attribute('src')
+        title = context.find_element_by_class_name('title').text
+        linkUrl = context.find_element_by_class_name('title').find_element_by_tag_name('a').get_attribute('href')
+        descriptContext = context.find_element_by_tag_name('p').text
+        pubDate = CommonsInitValue.initNowTime()
+        currentArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'STOCK','TAKCHINA'])
     return currentArray
      
 def writeFinanceHLDataSource():
     link = 'http://finance.takungpao.com/'
-    currentList = crawFinanceHLDataSource(link)
-    SQL = "DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'TAKCHINA' AND  NEWSFLAG='STOCK'"
+    currentArray = crawFinanceHLDataSource(link)
+
+    dbManager = CommonsMysqlUtils._dbManager
+    SQL = "DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE" \
+          "  WHERE  SOURCEFLAG = 'TAKCHINA' AND  NEWSFLAG='STOCK'"
+    dbManager.executeUpdateOrDelete(SQL)
 
     formatSQL = 'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE' \
                 ' (KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG)' \
                 ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
-
-if __name__ == '__main__':
-    writeFinanceHLDataSource()
+    dbManager.executeManyInsert(formatSQL,currentArray)
