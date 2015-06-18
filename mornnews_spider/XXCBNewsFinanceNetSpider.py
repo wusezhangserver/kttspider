468ac52f1df239ab7372131bf0fb900fdc94e64a
wusezhangserver <393124085@qq.com>
2015-06-18 13:30:10 +0800

    资源变更整理

--- mornnews_spider/XXCBNewsFinanceNetSpider.py
+++ mornnews_spider/XXCBNewsFinanceNetSpider.py
@@ -6,6 +6,7 @@ import uuid
 
 
 def crawMorningFinanceDailyNews(linkUrl):
+    currentArray = []
     browsor = webdriver.PhantomJS()
     browsor.get(linkUrl)
     maincontext = browsor.find_element_by_class_name('area_left')\
@@ -15,18 +16,23 @@ def crawMorningFinanceDailyNews(linkUrl):
         descriptContext = context.find_element_by_tag_name('p').text
         linkUrl = context.find_element_by_tag_name('a').get_attribute('href')
         title = context.find_element_by_tag_name('h2').text
-        print  title+":"+linkUrl+":"+imageUrl+":"+descriptContext
-    #currentArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'CHINA','XXCB'])
+        pubDate = CommonsInitValue.initNowTime()
+        currentArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'CHINA','XXCB'])
+    return currentArray
 
 
 def writeMorningFinanceDailyNews():
     link = 'http://www.xxcb.cn/event/caijing/'
     currentArray = crawMorningFinanceDailyNews(link)
+    dbManager = CommonsMysqlUtils._dbManager
     SQL = " DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'XXCB' " \
           " AND  NEWSFLAG='CHINA' "
+    dbManager.executeUpdateOrDelete(SQL)
+
     formatSQL = 'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE ' \
                 '(KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG)' \
                 ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+    dbManager.executeManyInsert(formatSQL,currentArray)
 
 if __name__ == '__main__':
     writeMorningFinanceDailyNews()
