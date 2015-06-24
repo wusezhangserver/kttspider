081ac82b44987d4f5fa090dd88bb6bc1c9e4781d
wusezhangserver <393124085@qq.com>
2015-06-24 15:50:52 +0800

    资源变更整理

--- common_spider_controller/dailytheme_spider_controller.py
+++ common_spider_controller/dailytheme_spider_controller.py
@@ -16,30 +16,50 @@ def  crawThemeNews():
 
     # CRAW THE IMPORT NEWS
     print '----START CRAW THE IMPORT NEWS----'
-    ImportantNewsSpider.writeCompanyNews()
+    try:
+       ImportantNewsSpider.writeCompanyNews()
+    except Exception, e:
+        currentList.append([currentTime,str(uuid.uuid1()),'ImportantNewsSpider.writeCompanyNews',e])
     
     #CRAW THE COMPANY NEWS
     print '----START CRAW THE COMPANY NEWS----'
-    CompanyNewsSpider.writeCompanyNews()
-    
+    try:
+        CompanyNewsSpider.writeCompanyNews()
+    except Exception, e:
+        currentList.append([currentTime,str(uuid.uuid1()),'CompanyNewsSpider.writeCompanyNews',e])
+
     #CRAW THE THEME NEWS
     print '----START CRAW THE THEME NEWS----'
-    ThemeNewsSpider.writeThemeDailyNews()
-    
+    try:
+        ThemeNewsSpider.writeThemeDailyNews()
+    except Exception, e:
+        currentList.append([currentTime,str(uuid.uuid1()),'ThemeNewsSpider.writeThemeDailyNews',e])
+
     #CRAW THE YICAI NEWS
     print '----START CRAW THE YICAI NEWS----'
-    YiCaiCompanyNewsSpider.writeYiCaiCompanyNews()
-    
+    try:
+        YiCaiCompanyNewsSpider.writeYiCaiCompanyNews()
+    except Exception, e:
+        currentList.append([currentTime,str(uuid.uuid1()),'YiCaiCompanyNewsSpider.writeYiCaiCompanyNews',e])
+
     #CRAW THE PWTHEME NEWS
     print '----START CRAW THE PWTHEME NEWS----'
-    PwThemeNewsSpider.writeDailyThemeNews()
-    
+    try:
+        PwThemeNewsSpider.writeDailyThemeNews()
+    except Exception, e:
+        currentList.append([currentTime,str(uuid.uuid1()),'PwThemeNewsSpider.writeDailyThemeNews',e])
+
     #CRAW THE CRICOMPANY NEWS
     print '----START CRAW THE CRICOMPANY NEWS----'
-    CriCompanyNewsSpider.writeDailyCompanyNews()
-    
+    try:
+        CriCompanyNewsSpider.writeDailyCompanyNews()
+    except Exception, e:
+        currentList.append([currentTime,str(uuid.uuid1()),'CriCompanyNewsSpider.writeDailyCompanyNews',e])
+
     #CRAW THE StcnTheme NEWS
     print '----START CRAW THE StcnTheme NEWS----'
-    StcnThemeNewsSpider.writeDailyThemeNews()
-
+    try:
+        StcnThemeNewsSpider.writeDailyThemeNews()
+    except Exception, e:
+        currentList.append([currentTime,str(uuid.uuid1()),'StcnThemeNewsSpider.writeDailyThemeNews',e])
     CommonsRecodeErrorUtils.commonRedcodeError(currentList)
